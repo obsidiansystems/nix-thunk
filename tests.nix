fe4b4dd2dbe833ac1ecbf1ec84d7f12e8ac36993
Amélia Liao <me@amelia.how>
2022-08-16 14:04:38 -0300

    test that dirty repos can not be packed; clean script up a bit

--- tests.nix
+++ tests.nix
@@ -72,17 +72,21 @@ in
       start_all()
 
       with subtest("nix-thunk is installed and git can be configured"):
-        client.succeed("nix-thunk --help")
-        client.succeed('git config --global user.email "you@example.com"')
-        client.succeed('git config --global user.name "Your Name"')
+        client.succeed("""
+          nix-thunk --help;
+          git config --global user.email "you@example.com";
+          git config --global user.name "Your Name";
+        """)
 
       githost.wait_for_open_port("22")
 
       with subtest("the clients can access the server via ssh"):
         for machine in [client, noNixThunk]:
-          machine.succeed("mkdir -p ~/.ssh/")
-          machine.succeed("cp ${privateKeyFile} ~/.ssh/id_rsa")
-          machine.succeed("chmod 600 ~/.ssh/id_rsa")
+          machine.succeed("""
+            mkdir -p ~/.ssh/;
+            cp ${privateKeyFile} ~/.ssh/id_rsa;
+            chmod 600 ~/.ssh/id_rsa;
+          """)
           machine.wait_until_succeeds(
             "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa githost true"
           )
@@ -90,49 +94,68 @@ in
           machine.wait_until_succeeds("ssh githost true")
 
       with subtest("a remote bare repo can be started"):
-        githost.succeed("mkdir -p ~/myorg/myapp.git")
-        githost.succeed("cd ~/myorg/myapp.git && git init --bare")
+        githost.succeed("""
+          mkdir -p ~/myorg/myapp.git;
+          cd ~/myorg/myapp.git && git init --bare
+        """)
 
       with subtest("a git project can be configured with a remote using ssh"):
-        client.succeed("mkdir -p ~/code/myapp")
-        client.succeed("cd ~/code/myapp && git init")
-        client.succeed("cp ${thunkableSample} ~/code/myapp/default.nix")
-        client.succeed("cd ~/code/myapp && git add .")
-
-        client.succeed('cd ~/code/myapp && git commit -m "Initial"')
-        client.succeed("cd ~/code/myapp && git remote add origin root@githost:/root/myorg/myapp.git")
+        client.succeed("""
+          mkdir -p ~/code/myapp;
+          cd ~/code/myapp;
+          git init;
+          cp ${thunkableSample} default.nix;
+          git add .;
+          git commit -m 'Initial';
+          git remote add origin root@githost:/root/myorg/myapp.git;
+        """)
 
       with subtest("pushing code to the remote"):
-        client.succeed("cd ~/code/myapp && git push -u origin master")
-        client.succeed("cd ~/code/myapp && git status")
+        client.succeed("""
+          cd ~/code/myapp;
+          git push -u origin master;
+          git status;
+        """)
 
       with subtest("nix-thunk can pack and unpack"):
-        client.succeed("nix-thunk pack ~/code/myapp")
-        client.succeed("grep -qF 'git.json' ~/code/myapp/thunk.nix")
-        client.succeed("grep -qF 'myorg' ~/code/myapp/git.json")
-        client.succeed("nix-thunk unpack ~/code/myapp")
+        client.succeed("""
+          nix-thunk pack ~/code/myapp;
+          grep -qF 'git.json' ~/code/myapp/thunk.nix;
+          grep -qF 'myorg' ~/code/myapp/git.json;
+          nix-thunk unpack ~/code/myapp;
+        """)
 
       with subtest("nix-thunk can create from ssh remote"):
-        client.succeed("nix-thunk pack ~/code/myapp")
-        client.succeed("nix-thunk create -b master root@githost:/root/myorg/myapp.git ~/code/myapp-remote")
-        client.succeed("diff -u ~/code/myapp/git.json ~/code/myapp-remote/git.json")
-        client.succeed("cmp ~/code/myapp/git.json ~/code/myapp-remote/git.json")
-        client.succeed("nix-thunk unpack ~/code/myapp; nix-thunk unpack ~/code/myapp-remote")
+        client.succeed("""
+          nix-thunk pack ~/code/myapp;
+          nix-thunk create -b master root@githost:/root/myorg/myapp.git ~/code/myapp-remote;
+          diff -u ~/code/myapp/git.json ~/code/myapp-remote/git.json;
+          cmp ~/code/myapp/git.json ~/code/myapp-remote/git.json;
+          nix-thunk unpack ~/code/myapp;
+          nix-thunk unpack ~/code/myapp-remote;
+        """)
 
       with subtest("nix-thunk can create from local directory"):
-        client.succeed("nix-thunk create ~/code/myapp ~/code/myapp-local")
-        client.succeed("nix-thunk unpack ~/code/myapp-local")
+        client.succeed("""
+          nix-thunk create ~/code/myapp ~/code/myapp-local
+          nix-thunk unpack ~/code/myapp-local
+        """)
 
       with subtest("unpacked thunks can be built"):
-        client.succeed("nix-build ~/code/myapp")
-        client.succeed("nix-build ~/code/myapp-remote")
-        client.succeed("nix-build ~/code/myapp-local")
+        client.succeed("""
+          nix-build ~/code/myapp;
+          nix-build ~/code/myapp-remote;
+          nix-build ~/code/myapp-local;
+        """)
 
       with subtest("packed thunks can be built"):
-        client.succeed("nix-thunk -v pack ~/code/myapp-remote; nix-thunk -v pack ~/code/myapp-local")
-        client.succeed("nix-build ~/code/myapp-remote")
-        client.succeed("nix-build ~/code/myapp-local")
-        client.succeed("nix-thunk unpack ~/code/myapp-remote")
+        client.succeed("""
+          nix-thunk -v pack ~/code/myapp-remote;
+          nix-thunk -v pack ~/code/myapp-local;
+          nix-build ~/code/myapp-remote;
+          nix-build ~/code/myapp-local;
+          nix-thunk unpack ~/code/myapp-remote;
+        """)
 
       with subtest("nix-thunk can update from ssh remote"):
         client.succeed("""
@@ -141,23 +164,28 @@ in
           git add test-file;
           git commit test-file -m "add test file";
           git push;
+
+          nix-thunk pack ~/code/myapp-remote;
+          nix-thunk update ~/code/myapp-remote;
+          nix-thunk unpack ~/code/myapp-remote;
+          test -f ~/code/myapp-remote/test-file;
         """)
-        client.succeed("nix-thunk pack ~/code/myapp-remote")
-        client.succeed("nix-thunk update ~/code/myapp-remote")
-        client.succeed("nix-thunk unpack ~/code/myapp-remote")
-        client.succeed("test -f ~/code/myapp-remote/test-file")
 
       with subtest("nix-thunk can update from local directory"):
-        client.succeed("nix-thunk update ~/code/myapp-local")
-        client.succeed("nix-thunk unpack ~/code/myapp-local")
-        client.succeed("test -f ~/code/myapp-local/test-file")
+        client.succeed("""
+          nix-thunk update ~/code/myapp-local;
+          nix-thunk unpack ~/code/myapp-local;
+          test -f ~/code/myapp-local/test-file;
+        """)
 
-      # This test is a bit... dirty. We can't create the thunk on
-      # 'noNixThunk', that's the whole point. We can't create the thunk
-      # in this nix file, because [mumble] nix-prefetch-git doesn't work
-      # inside the sandbox. But you know what we *can* do? Move the
-      # thunk from one machine to the other. And since we already have
-      # Git...
+      with subtest("nix-thunk pack will not destroy changes"):
+        client.succeed("""
+          cd ~/code/myapp-local;
+          echo "# Some change" >> default.nix;
+          nix-build
+        """);
+        client.fail("nix-thunk pack ~/code/myapp-local;")
+
       with subtest("packed thunks can be built without nix-thunk"):
         client.succeed("""
           nix-thunk pack ~/code/myapp-remote;
