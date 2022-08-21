fe7596752d7ec7c51bf8bdef571480c4704b27bd
Madeline Haraj <madeline.haraj@obsidian.systems>
2022-08-21 19:12:06 -0400

    Split utility functions out from default.nix
    nix-thunk has two consumers: those who want to build the tool as such,
    and those who want to use its nix functions to manipulate thunks. The
    utility nix functions have been split out into `lib.nix`.

--- default.nix
+++ default.nix
@@ -4,7 +4,14 @@
 
 with pkgs.haskell.lib;
 
-let inherit (pkgs) lib; in rec {
+rec {
+  inherit (import ./dep/gitignore.nix { inherit (pkgs) lib; }) gitignoreSource;
+  lib = pkgs.callPackage ./lib.nix {
+    inherit gitignoreSource;
+    inherit (pkgs) fetchgitPrivate;
+  };
+  inherit (lib) thunkSource mapSubdirectories;
+
   # The version of nixpkgs that we use for fetching packing thunks (by
   # themselves). Not to be used for building packages.
   packedThunkNixpkgs = builtins.fetchTarball {
@@ -105,52 +112,6 @@ let inherit (pkgs) lib; in rec {
 
   command = generateOptparseApplicativeCompletion "nix-thunk" (justStaticExecutables haskellPackages.nix-thunk);
 
-  inherit (import ./dep/gitignore.nix { inherit lib; }) gitignoreSource;
-
-  # Retrieve source that is controlled by the hack-* scripts; it may be either a stub or a checked-out git repo
-  thunkSource = p:
-    let
-      contents = builtins.readDir p;
-
-      contentsMatch = { required, optional }:
-           (let all = required // optional; in all // contents == all)
-        && builtins.intersectAttrs required contents == required;
-
-      # Newer obelisk thunks include the feature of hackGet with a thunk.nix file in the thunk.
-      isObeliskThunkWithThunkNix =
-        let
-          packed = jsonFileName: {
-            required = { ${jsonFileName} = "regular"; "default.nix" = "regular"; "thunk.nix" = "regular"; };
-            optional = { ".attr-cache" = "directory"; };
-          };
-        in builtins.any (n: contentsMatch (packed n)) [ "git.json" "github.json" ];
-
-      filterArgs = x: removeAttrs x [ "branch" ];
-      hasValidThunk = name: if builtins.pathExists (p + ("/" + name))
-        then
-          contentsMatch {
-            required = { ${name} = "regular"; };
-            optional = { "default.nix" = "regular"; ".attr-cache" = "directory"; };
-          }
-          || throw "Thunk at ${toString p} has files in addition to ${name} and optionally default.nix and .attr-cache. Remove either ${name} or those other files to continue (check for leftover .git too)."
-        else false;
-    in
-      if isObeliskThunkWithThunkNix then import (p + "/thunk.nix")
-      else if hasValidThunk "git.json" then (
-        let gitArgs = filterArgs (builtins.fromJSON (builtins.readFile (p + "/git.json")));
-        in if builtins.elem "@" (lib.stringToCharacters gitArgs.url)
-          then pkgs.fetchgitPrivate gitArgs
-          else pkgs.fetchgit gitArgs
-        )
-      else if hasValidThunk "github.json" then
-        pkgs.fetchFromGitHub (filterArgs (builtins.fromJSON (builtins.readFile (p + "/github.json"))))
-      else {
-        name = baseNameOf p;
-        outPath = gitignoreSource p;
-      };
-
-  #TODO: This really shouldn't include *all* symlinks, just ones that point at directories
-  mapSubdirectories = f: dir: lib.mapAttrs (name: _: f (dir + "/${name}")) (lib.filterAttrs (_: type: type == "directory" || type == "symlink") (builtins.readDir dir));
 
   ##############################################################################
   # Deprecated functions
